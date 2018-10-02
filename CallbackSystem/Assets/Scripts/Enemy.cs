b6bb01d01cc5803f105ef6e3ed11e7f253873493
krabbkakan <erikhede@gmail.com>
2018-10-02 16:46:30 +0200

    callback system with singelton
    callback system with singelton

---
+++ CallbackSystem/Assets/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Player.OnEnemyHit += Damage;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Damage(Color color) {
+        transform.GetComponent<Renderer>().material.color = color;
+    }
+
+    private void OnDisable()
+    {
+        Player.OnEnemyHit -= Damage;
+    }
+}
---
+++ CallbackSystem/Assets/Scripts/Player.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    
+    #region Singelton
+    private static Player _instance;
+
+    public static Player Instace
+    {
+        get
+        {
+
+            if (_instance == null)
+            {
+                GameObject go = new GameObject("Player");
+                go.AddComponent<Player>();
+            }
+
+            return _instance;
+
+        }
+
+    }
+    #endregion
+ 
+    public delegate void ChangeEnemyColor(Color color);
+    public static event ChangeEnemyColor OnEnemyHit;
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            // Damage enemy when space key is down
+
+            if (OnEnemyHit != null) {
+                OnEnemyHit(Color.red);
+            }
+
+
+        }
+
+
+    }
+
+
+}
